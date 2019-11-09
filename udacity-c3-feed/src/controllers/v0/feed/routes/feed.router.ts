61c722fc9d1521a493c7cc2d641f9a0f0feca40c
KugMax <maximkugatov@gmail.com>
2019-11-09 06:06:39 +0200

    refactor: splited restapi at feed, and user

--- udacity-c3-feed/src/controllers/v0/feed/routes/feed.router.ts
+++ udacity-c3-feed/src/controllers/v0/feed/routes/feed.router.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from 'express';
 import { FeedItem } from '../models/FeedItem';
-import { requireAuth } from '../../users/routes/auth.router';
 import * as AWS from '../../../../aws';
 
 const router: Router = Router();
@@ -26,7 +25,7 @@ router.get('/:id',
 
 // update a specific resource
 router.patch('/:id', 
-    requireAuth, 
+    // requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
         res.send(500).send("not implemented")
@@ -35,7 +34,7 @@ router.patch('/:id',
 
 // Get a signed url to put a new item in the bucket
 router.get('/signed-url/:fileName', 
-    requireAuth, 
+    // requireAuth, 
     async (req: Request, res: Response) => {
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
@@ -46,7 +45,7 @@ router.get('/signed-url/:fileName',
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
 router.post('/', 
-    requireAuth, 
+    // requireAuth, 
     async (req: Request, res: Response) => {
     const caption = req.body.caption;
     const fileName = req.body.url;
